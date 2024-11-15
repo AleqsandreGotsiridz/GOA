5fa1a1973a1ffb35b2fce5551416ff98291343c7
AleqsandreGotsiridz <gotsiridzealeqsandre@gmail.com>
2024-11-15 07:43:42 -0800

    commit

---
+++ Level 26/classwork/task.py
@@ -0,0 +1,8 @@
+#დაბეჭდეთ მეორე და მეოთხე ელემენტი ხუთი ფერის სიიდან, შემდეგ შეცვალეთ მესამე ელემენტი ახალ ფერში და დაბეჭდეთ განახლებული სია.
+colors = ["red","blue","green","yellow","purple"]
+print(colors[1])#blue
+
+print(colors[3])#yellow
+
+colors[2] = "black"
+print(colors)
