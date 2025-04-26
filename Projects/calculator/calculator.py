50307e9b2917ecd5fd1daf856122a94da104b080
AleqsandreGotsiridz <gotsiridzealeqsandre@gmail.com>
2025-04-26 02:52:29 -0700

    commit

--- Projects/calculator/calculator.py
+++ Projects/calculator/calculator.py
@@ -1,17 +1,20 @@
+
 operator = input("Enter the operator (+, -, *, //): ")
 num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
+num3 = float(input("Enter the 3nd number: "))
+numi = 5
 if operator == "+":
-        result = num1 + num2
-        print(round(result,3 ))
+        result = num1 + num2 + num3
+        print(round(result,4 ))
 elif operator == "-":
-        result = num1 - num2 
-        print(round(result,3 ))
+        result = num1 - num2 - num3
+        print(round(result,4 ))
 elif operator == "*":
-        result = num1 * num2
-        print(round(result,3 ))
+        result = num1 * num2 * num3
+        print(round(result,4 ))
 elif operator == "//":
-        result = num1 // num2
-        print(round(result,3 ))
+        result = num1 // num2 // num3
+        print(round(result,4 ))
 else:
         print(f"{operator} is not a valid operator! Please use one of the following: +, -, *, //")
