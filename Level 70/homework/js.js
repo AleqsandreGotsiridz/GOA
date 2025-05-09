a8c693497b987badf1e0a13157cc2a9cbd041b98
AleqsandreGotsiridz <gotsiridzealeqsandre@gmail.com>
2025-05-09 01:10:38 -0700

    commit

---
+++ Level 70/homework/js.js
@@ -0,0 +1,20 @@
+let my = {
+    name:"Sandro",
+    surname:"Gotsiridze",
+    age: 13,
+    land:"Goal-Oriented academy",
+    hobby:"playing on ect.Guitar"
+}
+console.log(my.name)
+console.log(my.surname)
+
+let best_academy = "Goa"
+console.log(best_academy.length())
+
+const mac = "donAlDs"
+console.log(mac.toUpperCase)
+console.log(mac.toLowerCase)
+// var/const/let
+// var-ი არის ყველაზე ძველი და საშინელი გზა ცვლადების შესაქმნელად და მას აღარ იყენებენ
+// const-ი არის უკვე ახალი გზა ცვლადების შესაქმნელად მაგრამ როცა ამით ვქმნით ცვლადს მის განახლება არ შეგვიძლია
+// let-იც უკვე ახალი გზაა მაგრამ  const ისგან განსხვავებით მისი შეცვლა შეგვიძლია
