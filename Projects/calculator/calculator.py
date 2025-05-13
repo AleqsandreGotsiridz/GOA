f160ddaa2e3a2ae3864587f00ae35f3b51ab8f7e
AleqsandreGotsiridz <gotsiridzealeqsandre@gmail.com>
2025-05-12 23:55:09 -0700

    commit

--- Projects/calculator/calculator.py
+++ Projects/calculator/calculator.py
@@ -1,19 +1,29 @@
-operator = input("Enter the operator (+, -, *, //): ")
-num1 = float(input("Enter the 1st number: "))
-num2 = float(input("Enter the 2nd number: "))
-num3 = float(input("Enter the 3nd number: "))
-numi = 5
-if operator == "+":
-        result = num1 + num2 + num3
-        print(round(result,4 ))
-elif operator == "-":
-        result = num1 - num2 - num3
-        print(round(result,4 ))
-elif operator == "*":
-        result = num1 * num2 * num3
-        print(round(result,4 ))
-elif operator == "//":
-        result = num1 // num2 // num3
-        print(round(result,4 ))
-else:
-        print(f"{operator} is not a valid operator! Please use one of the following: +, -, *, //")
+while True:      
+        operator = input("Enter the operator (+, -, *, //): ")
+        num1 = float(input("Enter the 1st number: "))
+        num2 = float(input("Enter the 2nd number: "))
+        num3 = float(input("Enter the 3nd number: "))
+        num4 = float(input("Enter the 4nd number:"))
+        numi = 5
+        if operator == "+":
+                result = num1 + num2 + num3
+                print(round(result,4 ))
+        elif operator == "-":
+                result = num1 - num2 - num3
+                print(round(result,4 ))
+        elif operator == "*":
+                result = num1 * num2 * num3
+                print(round(result,4 ))
+        elif operator == "//":
+                result = num1 // num2 // num3
+                print(round(result,4 ))
+        elif operator == "/":
+                if num2 == 0 or num3 == 0:
+                        print("Error__:")
+                else:
+                        result = num1/num2/num3
+                        print(round(result,4))
+        else:
+                print(f"{operator} is not a valid operator! Please use one of the following: +, -, *, //")
+# while True:
+#         again = input("Do you want to calculate  again?")
---
+++ Projects/calculator/goa.py
@@ -0,0 +1,13 @@
+Goa_studentsa_iq = {
+    "Nika": 140,
+    "Aleqs": 100,
+    "Gio": 70,
+}
+kid_iq_be = 140
+append = []
+input("Tell Me a Goa students Name")
+for i in Goa_studentsa_iq:
+    if kid_iq_be == Goa_studentsa_iq:
+        print(Goa_studentsa_iq.append(append))
+    else:
+        print("Yes iTs")
