6c5041e347918e3a5ae38905def1a15604062e59
AleqsandreGotsiridz <gotsiridzealeqsandre@gmail.com>
2025-05-28 23:58:54 -0700

    commit

---
+++ Level 76/classwork/js.js
@@ -0,0 +1,30 @@
+                                //1
+const year = 10
+
+if(year < 13){
+    console.log("you are a kid!")
+}else if(year > 13 && year < 18){
+    console.log("are you adult yet!")
+}else{
+    console.log("you re adult!")
+}
+                                //2
+let num = 35
+
+if(num / 2 == 0){
+    console.log("Divided into equal parts")
+}else{
+    console.log("num is odd")
+}
+                                //3
+//in py
+                                //4
+"!"
+//ეს ნიშანი აღნიშნავს უტოლობას
+                                //5
+const ll = 27
+if(year / 3 || 9 == 0 ){
+    console.log(ll)
+}else {
+    console.log("task was not completed.")
+}
---
+++ Level 76/classwork/python.py
@@ -0,0 +1,13 @@
+asaki = 12
+
+if asaki < 13:
+    print("you re kid!")
+elif asaki > 13 and asaki<18:
+    print("you are adult yet")
+else:
+    print("you are adult!")
+
+#python and js ერთმანეთისგან გამოიყენება იმით რომ python არის 
+# backend-ი ხოლო js არის frontend ასევე js გამოიყენება საიტების 
+# ინტერფეისისთვის და ასევე მათ შორის დიდზე განსხვავდება სინტაქსი 
+# და პითონი ძირითადად კალკულატორი ან ai sTvisac გამოიყენება ხოლმე. 
