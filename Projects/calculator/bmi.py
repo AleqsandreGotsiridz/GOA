aef206a69a5ba38d97a7b1020d176dd390b09e4e
AleqsandreGotsiridz <gotsiridzealeqsandre@gmail.com>
2025-06-19 19:51:24 -0700

    commit

--- Projects/calculator/bmi.py
+++ Projects/calculator/bmi.py
@@ -1,17 +1,18 @@
     #Bmi
-Input = input("Hi it's a BMI Lets GO!:")#input
-age = float(input("enter your age:"))#age
-weight = float(input("enter your weigth:"))#weight
-height = float(input("enter your heigth:"))#height
-heigth_in_meters = height / 100#height in meters 
-bmi = weight / (heigth_in_meters ** 2)#squaring
-print("your bmi is:",bmi)#print
+while True:
+    Input = input("Hi it's a BMI Lets GO!:")#input
+    age = float(input("enter your age:"))#age
+    weight = float(input("enter your weigth:"))#weight
+    height = float(input("enter your heigth:"))#height
+    heigth_in_meters = height / 100#height in meters 
+    bmi = weight / (heigth_in_meters ** 2)#squaring
+    print("your bmi is:",bmi)#print
 
-if bmi < 18.5:
-    print("You're underweight!")
-elif 18.5 <= bmi < 25:
-    print("You're a normal weight!")
-elif 25 <= bmi < 30:
-    print("You're overweight!")
-else:
-    print("You're fat!")
+    if bmi < 18.5:
+        print("You're underweight!")
+    elif 18.5 <= bmi < 25:
+        print("You're a normal weight!")
+    elif 25 <= bmi < 30:
+        print("You're overweight!")
+    else:
+        print("You're fat!")
